d2f412daa744ed859385800e9b4a36dbc866f7d9
RobertoChiosa <roberto.chiosa@gmail.com>
2021-05-04 17:29:49 +0200

    reproducible research plots done

---
+++ 00-utils-functions.R
@@ -0,0 +1,212 @@
+
+plot_sequence <- function(type = "data",
+                          dataset,
+                          x,
+                          x_lab = NULL,
+                          y,
+                          y_lab = NULL,
+                          ymax_mp = NULL,
+                          mp_index = NULL,
+                          w = NULL,
+                          seq_index = NULL,
+                          seq_nn = NULL
+) 
+{
+  fontsize <- 11
+  seq_color <- "green"
+  nn_color <- "blue"
+  
+  plot <- ggplot(dataset, aes_string(x = x, y = y)) +
+    geom_line() +
+    theme_bw() +
+    theme(
+      plot.title = element_markdown(lineheight = 1.1),
+      panel.background = element_rect(fill = "white"),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.title = element_text(size = fontsize),
+      axis.title.x = element_text(
+        size = fontsize,
+        margin = margin(
+          t = 1,
+          r = 1,
+          b = 0,
+          l = 0,
+          unit = "mm"
+        )
+      ),
+      axis.title.y = element_text(
+        size = fontsize,
+        margin = margin(
+          t = 1,
+          r = 1,
+          b = 0,
+          l = 0,
+          unit = "mm"
+        )
+      ),
+      axis.text.x = element_text(size = fontsize),
+      axis.text.y = element_text(size = fontsize)
+    ) + 
+    labs(x = x_lab, y = y_lab)
+  
+  if (type == "data") {
+    # in this case draw sequence rectabgle starting in the index
+    plot <- plot +
+      annotate(
+        "rect",
+        xmin = dataset[[x]][seq_index],
+        xmax = dataset[[x]][seq_index + w],
+        ymin = 0,
+        ymax = max(dataset[[y]]),
+        alpha = .5,
+        fill = seq_color
+      ) +
+      # nearest neighbor
+      annotate(
+        "rect",
+        xmin = dataset[[x]][seq_nn],
+        xmax = dataset[[x]][seq_nn + w],
+        ymin = 0,
+        ymax = max(dataset[[y]]),
+        alpha = .5,
+        fill = nn_color
+      ) 
+    
+  }
+  
+  if (type == "mp") {
+    # in this case draw a line
+    plot <- plot +
+      geom_vline(xintercept = dataset[[x]][seq_index],
+                 color = seq_color,
+                 size = 0.5) +
+      # nearest neighbor
+      geom_vline(xintercept = dataset[[mp_index]][seq_index],
+                 color = nn_color,
+                 size = 0.5) +
+      scale_y_continuous(limits = c(0, ymax_mp )) +
+      labs(title = paste("<span align='center' style='font-size:11pt'>
+      <span style='color:green;'>Sequence</span> [MP, MP_index] = [<span style='color:green;'>",round( dataset[[y]][seq_index] ,3) ,";", dataset[[x]][seq_index], "</span>] -
+      <span style='color:blue;'>1stNN</span> [MP, MP_index] = [<span style='color:blue;'>",round(dataset[[y]][dataset[[mp_index]][seq_index]  ],3) ,";", dataset[[mp_index]][seq_index], "</span>] 
+                         </span>"
+      ))
+    
+    if (dataset[[mp_index]][seq_index] > dataset[[x]][seq_index] ) { # rigth
+      plot <- plot + 
+        annotate("rect", 
+                 xmin = dataset[[x]][seq_index], 
+                 xmax = max( dataset[[x]]),
+                 ymin = 0, 
+                 ymax = ymax_mp,
+                 alpha = .3,fill = "gray")
+      
+    } else {
+      plot <- plot + 
+        annotate("rect", 
+                 xmin = dataset[[mp_index]][seq_index],
+                 xmax = min( dataset[[x]]),
+                 ymin = 0, 
+                 ymax = ymax_mp,
+                 alpha = .3,fill = "gray")
+    }
+    
+    
+    
+    
+  } 
+  
+  return(plot)
+  
+}
+
+
+plot_window <- function(type = "pure",
+                        dataset,
+                        x,
+                        x_lab = NULL,
+                        y,
+                        y_lab = NULL,
+                        w = NULL,
+                        seq_index = NULL,
+                        seq_nn = NULL
+) 
+{
+  fontsize <- 11
+  seq_color <- "green"
+  nn_color <- "blue"
+  
+  
+  plot <- ggplot(dataset) 
+  
+  if (type == "pure") {
+    plot <- plot + 
+      geom_line(
+      data = dataset %>% filter(index >= seq_index,
+                                index <= seq_index +  w) ,
+      aes(x = c(0:w), y = data),
+      color = "green",
+      size = 1.5
+    ) +
+      geom_line(
+        data = dataset %>% filter(index >= seq_nn,
+                                  index <= seq_nn +  w) ,
+        aes(x = c(0:w), y = data),
+        color = "blue",
+        size = 1.5
+      ) 
+  } else if (type == "znorm") {
+    
+    plot <- plot + 
+      geom_line(
+        data = dataset %>% filter(index >= seq_index,
+                                  index <= seq_index +  w) ,
+        aes(x = c(0:w), (data - mean(data)) / sd(data)),
+        color = "green",
+        size = 1.5
+      ) +
+      geom_line(
+        data = dataset %>% filter(index >= seq_nn,
+                                  index <= seq_nn +  w) ,
+        aes(x = c(0:w), (data - mean(data)) / sd(data)),
+        color = "blue",
+        size = 1.5
+      ) 
+  }
+    
+  plot <- plot + 
+    theme_bw() +
+    theme(
+      plot.title = element_markdown(lineheight = 1.1),
+      panel.background = element_rect(fill = "white"),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      legend.title = element_text(size = fontsize),
+      axis.title.x = element_text(
+        size = fontsize,
+        margin = margin(
+          t = 1,
+          r = 1,
+          b = 0,
+          l = 0,
+          unit = "mm"
+        )
+      ),
+      axis.title.y = element_text(
+        size = fontsize,
+        margin = margin(
+          t = 1,
+          r = 1,
+          b = 0,
+          l = 0,
+          unit = "mm"
+        )
+      ),
+      axis.text.x = element_text(size = fontsize),
+      axis.text.y = element_text(size = fontsize)
+    ) + 
+    labs(x = x_lab, y = y_lab)
+  
+  return(plot)
+  
+}
--- 01-preliminary-analysis.R
+++ 01-preliminary-analysis.R
@@ -1,129 +1,16 @@
+#  LOAD PACKAGES and FUNCTIONS ------------------------------------------------------------------
+cat("\014")             # clears the console
+rm(list = ls())         # remove all variables of the workspace
+library(energydataset)  # energy dataset to test
+library(tsmp)           # matrix profile dataset
+library(dplyr)          # dataset handling
+library(ggplot2)        # plot
+library(ggpubr)         # arrange plots
+library(ggtext)         # annotate text
 
+source(file = "00-utils-functions.R") # load user functions
 
-library(energydataset)
-library(tsmp)
-library(dplyr)
-library(ggplot2)
-library(ggpubr)
-library(ggtext) 
-
-# style definition for time series plot
-style <- {theme_bw() +
-    theme(
-      panel.background = element_rect(fill = "white"),
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      legend.title = element_text(size = 11),
-      axis.title.x = element_text(
-        size = 11,
-        margin = margin(
-          t = 1,
-          r = 1,
-          b = 0,
-          l = 0,
-          unit = "mm"
-        )
-      ),
-      axis.title.y = element_text(
-        size = 11,
-        margin = margin(
-          t = 1,
-          r = 1,
-          b = 0,
-          l = 0,
-          unit = "mm"
-        )
-      ),
-      axis.text.x = element_text(size = 11),
-      axis.text.y = element_text(size = 11)
-    )}
-
-plot_sequence <- function(type = "data", dataset, x, y, y_lab = NULL, mp_index = NULL, w, seq_index = NULL, seq_nn = NULL){
-  
-  plot <- ggplot(dataset, aes_string(x = x, y = y)) +
-    geom_line() +
-    theme_bw() +
-    theme(
-      plot.title = element_markdown(lineheight = 1.1),
-      panel.background = element_rect(fill = "white"),
-      panel.grid.major = element_blank(),
-      panel.grid.minor = element_blank(),
-      legend.title = element_text(size = 11),
-      axis.title.x = element_text(
-        size = 11,
-        margin = margin(
-          t = 1,
-          r = 1,
-          b = 0,
-          l = 0,
-          unit = "mm"
-        )
-      ),
-      axis.title.y = element_text(
-        size = 11,
-        margin = margin(
-          t = 1,
-          r = 1,
-          b = 0,
-          l = 0,
-          unit = "mm"
-        )
-      ),
-      axis.text.x = element_text(size = 11),
-      axis.text.y = element_text(size = 11)
-    )
-  
-  if (type == "data") {
-    
-    plot <- plot +
-      annotate(
-        "rect",
-        xmin = dataset[[x]][seq_index],
-        xmax = dataset[[x]][seq_index + w],
-        ymin = 0,
-        ymax = max(dataset[[y]]),
-        alpha = .5,
-        fill = "green"
-      ) +
-      # nearest neighbor
-      annotate(
-        "rect",
-        xmin = dataset[[x]][seq_nn],
-        xmax = dataset[[x]][seq_nn + w],
-        ymin = 0,
-        ymax = max(dataset[[y]]),
-        alpha = .5,
-        fill = "blue"
-      )
-    
-  }
-  
-  if (type == "mp") {
-    plot <- plot +
-      geom_vline(xintercept = dataset[[x]][seq_index],
-                 color = "green",
-                 size = 0.5) +
-      # nearest neighbor
-      geom_vline(xintercept = dataset[[mp_index]][seq_nn],
-                 color = "blue",
-                 size = 0.5) +
-      labs(x = NULL, 
-           y = y_lab,
-           title = paste(" **Matrix Profile**
-             <span style='font-size:13pt'> 
-             <span style='color:green;'>MP(Sequence)=", round(dataset[[y]][seq_index],3) ,"</span>,
-             <span style='color:blue;'>MP(1st Nearest Neighbor)=", round(dataset[[y]][seq_nn],3)  ," </span>,
-             </span>"
-           )
-      ) 
-  } 
-  if (type == "raw") {
-    plot <- plot + labs(x = NULL, y = y_lab)
-  }
-  
-  return(plot)
-  
-}
+#  PREPROCESSING ------------------------------------------------------------------
 
 # load dataset
 df <- energydataset::data_power_raw
@@ -145,35 +32,78 @@ df_univariate <- df %>%
 
 # plot dataset time series
 {
-  p1_TS <- ggplot(df_univariate, aes(x = CET, y = Power_total)) +
-    geom_line() + labs(x = NULL , y = "Total [kW]") + style
+  p1_TS <-  plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_total",
+    y_lab = "Total [kW]"
+  )
   
-  p2_TS <-
-    ggplot(df_univariate, aes(x = CET, y = Power_mechanical_room)) +
-    geom_line() + labs(x = NULL , y = "Mechanical room [kW]") + style
+  p2_TS <- plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_mechanical_room",
+    y_lab = "Mechanical room [kW]"
+  )
   
-  p3_TS <-
-    ggplot(df_univariate, aes(x = CET, y = Power_data_centre)) +
-    geom_line() + labs(x = NULL , y = "Data Centre [kW]") + style
+  p3_TS <-  plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_data_centre",
+    y_lab = "Data Centre [kW]"
+  )
   
-  p4_TS <-
-    ggplot(df_univariate, aes(x = CET, y = Power_canteen)) +
-    geom_line() + labs(x = NULL , y = "Canteen [kW]") + style
   
-  p5_TS <-
-    ggplot(df_univariate, aes(x = CET, y = Power_dimat)) +
-    geom_line() + labs(x = NULL , y = "DIMAT [kW]") + style
+  p4_TS <- plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_canteen",
+    y_lab = "Canteen [kW]"
+  )
   
-  p6_TS <- ggplot(df_univariate, aes(x = CET, y = Power_bar)) +
-    geom_line() + labs(x = NULL , y = "Bar [kW]") + style
+  p5_TS <- plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_dimat",
+    y_lab = "DIMAT [kW]"
+  )
   
-  p7_TS <-
-    ggplot(df_univariate, aes(x = CET, y = Power_rectory)) +
-    geom_line() + labs(x = NULL , y = "Rectory [kW]") + style
+  p6_TS <- plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_bar",
+    y_lab = "Bar [kW]"
+  )
   
-  p8_TS <-
-    ggplot(df_univariate, aes(x = CET, y = Power_print_shop)) +
-    geom_line() + labs(x = NULL , y = "Print Shop [kW]") + style
+  p7_TS <- plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_rectory",
+    y_lab = "Rectory [kW]"
+  )
+  
+  p8_TS <- plot_sequence(
+    type = "raw",
+    df_univariate,
+    x = "CET",
+    x_lab = NULL,
+    y = "Power_print_shop",
+    y_lab = "Print Shop [kW]"
+  )
   
   dev.new()
   
@@ -198,12 +128,13 @@ df_univariate <- df %>%
   dev.off()
   }
 
-# matrix profile on the total power
-w = 96
+#  MATRIX PROFILE ------------------------------------------------------------------
+#  matrix profile on the total electrical power
+w = 96 # window size
 # mp_univariate <- tsmp(df_univariate$Power_total, window_size = w, exclusion_zone = 0.5 )
 
 # save(mp_univariate, file = "./data/mp_univariate_total_05ex_w96.RData")
-load("./data/mp_univariate_total_05ex_w96.RData")
+load("./data/mp_univariate_total_05ex_w96.RData") # load to save time
 
 # define length of mp
 mp_length = length(mp_univariate$mp)
@@ -219,7 +150,7 @@ df_mp_univariate <- data.frame(
   lmp = mp_univariate[[5]],
   lmp_index = mp_univariate[[6]]
 )
-
+head(df_mp_univariate)
 
 # plot matrix profile
 {
@@ -229,6 +160,7 @@ df_mp_univariate <- data.frame(
     type = "raw",
     df_mp_univariate,
     x = "data_index",
+    x_lab = NULL,
     y = "data",
     y_lab = "Total [kW]",
     w = 96,
@@ -240,6 +172,7 @@ df_mp_univariate <- data.frame(
     type = "raw",
     df_mp_univariate,
     x = "index",
+    x_lab = NULL,
     y = "mp",
     y_lab = "MP",
     w = 96,
@@ -251,6 +184,7 @@ df_mp_univariate <- data.frame(
     type = "raw",
     df_mp_univariate,
     x = "index",
+    x_lab = NULL,
     y = "rmp",
     y_lab = "RMP",
     w = 96,
@@ -262,6 +196,7 @@ df_mp_univariate <- data.frame(
     type = "raw",
     df_mp_univariate,
     x = "index",
+    x_lab = NULL,
     y = "lmp",
     y_lab = "LMP",
     w = 96,
@@ -290,16 +225,19 @@ df_mp_univariate <- data.frame(
   
 }
 
-############################################ find discord
+#  SEQUENCE DISCOVERY ------------------------------------------------------------------
+#  https://matrixprofile.org/tsmp/reference/find_discord.html
 discord <- find_discord(mp_univariate,
                         n_discords = 1,
-                        n_neighbors = 1)
+                        n_neighbors = 1
+                        )
 
 # discord index
-discord_index <- as.numeric(discord$discord$discord_idx)
+sequence_index <- as.numeric(discord$discord$discord_idx)
 
+#  PLOTS ------------------------------------------------------------------
 # first nn index
-discord_n1_index <- as.numeric(discord$discord$discord_neighbor[[1]])
+# df_mp_univariate$lmp_index[sequence_index]
 
 # plot discord
 {
@@ -308,52 +246,59 @@ discord_n1_index <- as.numeric(discord$discord$discord_neighbor[[1]])
     type = "data",
     df_mp_univariate,
     x = "data_index",
+    x_lab = NULL,
     y = "data",
     y_lab = "Power [kW]",
     w = 96,
-    seq_index = discord_index,
-    seq_nn = discord_n1_index
+    seq_index = sequence_index,
+    seq_nn = df_mp_univariate$mp_index[sequence_index]
   )
   
   p1mp_discord <-  plot_sequence(
     type = "mp",
     df_mp_univariate,
     x = "index",
+    x_lab = NULL,
     y = "mp",
     y_lab = "MP",
+    ymax_mp = 20,
     mp_index = "mp_index",
     w = 96,
-    seq_index = discord_index,
-    seq_nn = discord_n1_index
+    seq_index = sequence_index,
+    seq_nn = df_mp_univariate$mp_index[sequence_index]
   )
   
   p2mp_discord <-  plot_sequence(
     type = "mp",
     df_mp_univariate,
     x = "index",
+    x_lab = NULL,
     y = "rmp",
     y_lab = "RMP",
+    ymax_mp = 20,
     mp_index = "rmp_index",
     w = 96,
-    seq_index = discord_index,
-    seq_nn = discord_n1_index
+    seq_index = sequence_index,
+    seq_nn = df_mp_univariate$rmp_index[sequence_index]
   )
   
   p3mp_discord <-   plot_sequence(
     type = "mp",
     df_mp_univariate,
     x = "index",
+    x_lab = NULL,
     y = "lmp",
     y_lab = "LMP",
+    ymax_mp = 20,
     mp_index = "lmp_index",
-    w = 96,
-    seq_index = discord_index,
-    seq_nn = discord_n1_index
+    w = 96, 
+    seq_index = sequence_index,
+    seq_nn = df_mp_univariate$lmp_index[sequence_index]
   )
   
   dev.new()
   
-  ggarrange(
+  fig <- ggarrange(
     p0data_discord,
     p1mp_discord,
     p2mp_discord,
@@ -364,6 +309,9 @@ discord_n1_index <- as.numeric(discord$discord$discord_neighbor[[1]])
     align = "v"
   )
   
+  annotate_figure(fig, top = text_grob("Discord discovery", color = "black", face = "bold", size = 13))
+                  
+                  
   ggsave("./figures/01.03-total-MP-discord.png",
          width = 10,
          height = 7)
@@ -372,50 +320,92 @@ discord_n1_index <- as.numeric(discord$discord$discord_neighbor[[1]])
 
 # plot discord profiles normal and normalized
 {
-  pmp_discord_pure <- ggplot() +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= discord_index,
-                                         index <= discord_index +  w) ,
-      aes(x = c(0:w), y = data),
-      color = "green",
-      size = 1.5
-    ) +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= discord_n1_index,
-                                         index <= discord_n1_index +  w) ,
-      aes(x = c(0:w), y = data),
-      color = "blue",
-      size = 1.5
-    ) +
-    labs(x = NULL , y = "Power [kW]") + style
+  pmp_discord_pure <-  plot_window(
+    type = "pure",
+    df_mp_univariate,
+    x = "index",
+    x_lab = NULL,
+    y = "data",
+    y_lab = NULL,
+    w = 96, 
+    seq_index = sequence_index ,
+    seq_nn =  df_mp_univariate$mp_index[sequence_index]
+  ) + labs( title="Power [kW]")
   
-  pmp_discord_z <- ggplot() +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= discord_index,
-                                         index <= discord_index +  w) ,
-      aes(x = c(0:w), y = (data - mean(data)) / sd(data)),
-      color = "green",
-      size = 1.5
-    ) +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= discord_n1_index,
-                                         index <= discord_n1_index +  w) ,
-      aes(x = c(0:w), y = (data - mean(data)) / sd(data)),
-      color = "blue",
-      size = 1.5
-    ) +
-    labs(x = NULL , y = "Power [z-score]") + style
+  pmp_discord_z <-  plot_window(
+    type = "znorm",
+    df_mp_univariate,
+    x = "index",
+    x_lab = NULL,
+    y = "data",
+    y_lab = NULL,
+    w = 96, 
+    seq_index = sequence_index,
+    seq_nn = df_mp_univariate$mp_index[sequence_index]
+  )+ labs( title="Power [znorm]")
+  
+  prmp_discord_pure <-  plot_window(
+    type = "pure",
+    df_mp_univariate,
+    x = "index",
+    x_lab = NULL,
+    y = "data",
+    y_lab = NULL,
+    w = 96, 
+    seq_index = sequence_index ,
+    seq_nn =  df_mp_univariate$rmp_index[sequence_index]
+  )
+  
+  prmp_discord_z <-  plot_window(
+    type = "znorm",
+    df_mp_univariate,
+    x = "index",
+    x_lab = NULL,
+    y = "data",
+    y_lab = NULL,
+    w = 96, 
+    seq_index = sequence_index,
+    seq_nn = df_mp_univariate$rmp_index[sequence_index]
+  )
+  
+  plmp_discord_pure <-  plot_window(
+    type = "pure",
+    df_mp_univariate,
+    x = "index",
+    x_lab = NULL,
+    y = "data",
+    y_lab = NULL,
+    w = 96, 
+    seq_index = sequence_index ,
+    seq_nn =  df_mp_univariate$lmp_index[sequence_index]
+  )
+  
+  plmp_discord_z <-  plot_window(
+    type = "znorm",
+    df_mp_univariate,
+    x = "index",
+    x_lab = NULL,
+    y = "data",
+    y_lab = NULL,
+    w = 96, 
+    seq_index = sequence_index,
+    seq_nn = df_mp_univariate$lmp_index[sequence_index]
+  )
   
   dev.new()
   
-  ggarrange(
-    pmp_discord_pure,
-    pmp_discord_z,
-    ncol = 1,
-    nrow = 2,
-    widths = c(3),
+  fig <-  ggarrange(
+    pmp_discord_pure, pmp_discord_z,
+    prmp_discord_pure, prmp_discord_z,
+    plmp_discord_pure, plmp_discord_z,
+    ncol = 2,
+    nrow = 3,
+    widths = c(3,3),
     align = "v"
   )
+  
+  annotate_figure(fig, top = text_grob("Discord discovery window profile", color = "black", face = "bold", size = 13))
+  
   
   ggsave(
     "./figures/01.04-total-MP-discord-profile.png",
@@ -425,211 +415,3 @@ discord_n1_index <- as.numeric(discord$discord$discord_neighbor[[1]])
   dev.off()
 }
 
-############################################ find motif
-motif <- find_motif(mp_univariate,
-                    n_motifs  = 1,
-                    n_neighbors = 1)
-
-# motif index
-motif_index <- as.numeric(motif$motif$motif_idx[[1]][1])
-
-# first nn index
-motif_n1_index <- as.numeric(motif$motif$motif_neighbor[[1]])
-
-# plot motif
-{
-  motif_df <- data.frame(
-    data_index = c(
-      df_mp_univariate$data_index[motif_index],
-      df_mp_univariate$data_index[motif_n1_index + w]
-    ),
-    data = c(max(df_mp_univariate$data),
-             max(df_mp_univariate$data)) - 50,
-    label = c("Motif", "1st NN")
-    
-  )
-  
-  p0data_motif <-
-    ggplot(df_mp_univariate, aes(x = data_index, y = data)) +
-    geom_line() + labs(x = NULL , y = "Total [kW]") + style +
-    # motif
-    annotate(
-      "rect",
-      xmin = df_mp_univariate$data_index[motif_index],
-      xmax = df_mp_univariate$data_index[motif_index + w],
-      ymin = 0,
-      ymax = max(df_mp_univariate$data),
-      alpha = .5,
-      fill = "green"
-    ) +
-    # nearest neighbor
-    annotate(
-      "rect",
-      xmin = df_mp_univariate$data_index[motif_n1_index],
-      xmax = df_mp_univariate$data_index[motif_n1_index + w],
-      ymin = 0,
-      ymax = max(df_mp_univariate$data),
-      alpha = .5,
-      fill = "blue"
-    ) +
-    geom_label(data = motif_df, aes(label = label))
-  
-  
-  p1mp_motif <-
-    ggplot(df_mp_univariate, aes(x = index, y = mp)) +
-    geom_line() + labs(x = NULL , y = "MP ") + style +
-    scale_x_continuous(limits = c(0, max(df_mp_univariate$index)),
-                       breaks = seq(0, max(df_mp_univariate$index), by = 1000)) +
-    scale_y_continuous(limits = c(0, mplimity)) +
-    # motif
-    annotate(
-      "rect",
-      xmin = motif_index,
-      xmax = motif_index + w,
-      ymin = 0,
-      ymax = mplimity,
-      alpha = .5,
-      fill = "green"
-    ) +
-    # nearest neighbor
-    annotate(
-      "rect",
-      xmin = motif_n1_index,
-      xmax = motif_n1_index + w,
-      ymin = 0,
-      ymax = mplimity,
-      alpha = .5,
-      fill = "blue"
-    )
-  
-  
-  
-  p2mp_motif <-
-    ggplot(df_mp_univariate, aes(x = index, y = rmp)) +
-    geom_line() +  labs(x = NULL , y = "RMP ") + style +
-    scale_x_continuous(limits = c(0, max(df_mp_univariate$index)),
-                       breaks = seq(0, max(df_mp_univariate$index), by = 1000)) +
-    scale_y_continuous(limits = c(0, mplimity)) +
-    # motif
-    annotate(
-      "rect",
-      xmin = motif_index,
-      xmax = motif_index + w,
-      ymin = 0,
-      ymax = mplimity,
-      alpha = .5,
-      fill = "green"
-    ) +
-    # nearest neighbor
-    annotate(
-      "rect",
-      xmin = motif_n1_index,
-      xmax = motif_n1_index + w,
-      ymin = 0,
-      ymax = mplimity,
-      alpha = .5,
-      fill = "blue"
-    )
-  
-  
-  p3mp_motif <-
-    ggplot(df_mp_univariate, aes(x = index, y = lmp)) +
-    geom_line() +  labs(x = NULL , y = "LMP") + style +
-    scale_x_continuous(limits = c(0, max(df_mp_univariate$index)),
-                       breaks = seq(0, max(df_mp_univariate$index), by = 1000)) +
-    scale_y_continuous(limits = c(0, mplimity)) +
-    # motif
-    annotate(
-      "rect",
-      xmin = motif_index,
-      xmax = motif_index + w,
-      ymin = 0,
-      ymax = mplimity,
-      alpha = .5,
-      fill = "green"
-    ) +
-    # nearest neighbor
-    annotate(
-      "rect",
-      xmin = motif_n1_index,
-      xmax = motif_n1_index + w,
-      ymin = 0,
-      ymax = mplimity,
-      alpha = .5,
-      fill = "blue"
-    )
-  
-  
-  dev.new()
-  
-  ggarrange(
-    p0data_motif,
-    p1mp_motif,
-    p2mp_motif,
-    p3mp_motif,
-    ncol = 1,
-    nrow = 4,
-    widths = c(3),
-    align = "v"
-  )
-  
-  ggsave("./figures/01.03-total-MP-motif.png",
-         width = 10,
-         height = 7)
-  dev.off()
-}
-
-# plot motif profiles normal and normalized
-{
-  pmp_motif_pure <- ggplot() +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= motif_index,
-                                         index <= motif_index +  w) ,
-      aes(x = c(0:w), y = data),
-      color = "green",
-      size = 1.5
-    ) +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= motif_n1_index,
-                                         index <= motif_n1_index +  w) ,
-      aes(x = c(0:w), y = data),
-      color = "blue",
-      size = 1.5
-    ) +
-    labs(x = NULL , y = "Power [kW]") + style
-  
-  pmp_motif_z <- ggplot() +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= motif_index,
-                                         index <= motif_index +  w) ,
-      aes(x = c(0:w), y = (data - mean(data)) / sd(data)),
-      color = "green",
-      size = 1.5
-    ) +
-    geom_line(
-      data = df_mp_univariate %>% filter(index >= motif_n1_index,
-                                         index <= motif_n1_index +  w) ,
-      aes(x = c(0:w), y = (data - mean(data)) / sd(data)),
-      color = "blue",
-      size = 1.5
-    ) +
-    labs(x = NULL , y = "Power [z-score]") + style
-  
-  dev.new()
-  
-  ggarrange(
-    pmp_motif_pure,
-    pmp_motif_z,
-    ncol = 1,
-    nrow = 2,
-    widths = c(3),
-    align = "v"
-  )
-  
-  ggsave(
-    "./figures/01.04-total-MP-motif-profile.png",
-    width = 10,
-    height = 7
-  )
-  dev.off()
-}
