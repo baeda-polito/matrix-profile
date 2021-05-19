8bda293328f4259d5666582c9e5766c3d9a57cbe
Simvit17 <84042272+Simvit17@users.noreply.github.com>
2021-05-19 17:31:09 +0200

    Update make_AV.R

--- Simone Vitale/make_AV.R
+++ Simone Vitale/make_AV.R
@@ -1,19 +1,62 @@
 # a<- sample(1:10,30,replace = T)
 # w <-5
 
-make_AV <- function(data, subsequenceLength){
+make_AV <- function(data, subsequenceLength, type=c('motionartifact','complexity'), binary=T){
   
+<<<<<<< Updated upstream
   AV = 0*c(1:length(data)-subsequenceLength+1)
   stdVector= 0*c(1:length(data)-subsequenceLength+1)
   
   for (bb in 1:(length(data)-subsequenceLength+1)){
+=======
+  AV = 0*c(1:(length(data)-subsequenceLength+1))
+  stdVector= 0*c(1:(length(data)-subsequenceLength+1))
+  
+  if(type=='motionartifact'){
     
-    stdVector[bb] = sd( data[ c(bb:(bb+subsequenceLength-1)) ] )
+    for (bb in 1:(length(data)-subsequenceLength+1)){
+      
+      stdVector[bb] = sd( data[ c(bb:(bb+subsequenceLength-1)) ] )
+      
+    }
+    meanstdV <-mean(stdVector)
+>>>>>>> Stashed changes
     
+    if(binary==T){
+      
+      AV =ifelse(stdVector >=meanstdV,0,1)
+      
+    }else{
+      
+      AV<-stdVector/max(stdVector)
+      
+    }
+    
+    return(list(AV<-AV,stdVector<-stdVector,meanstdV<-meanstdV))
   }
+<<<<<<< Updated upstream
   meanstdV <- mean(stdVector)
   
   AV = ifelse(stdVector >= meanstdV,1,0)
+=======
+  
+  if(type=='complexity'){
+    
+    for (bb in 1:(length(data)-subsequenceLength+1)){
+      
+      subsequence=data[bb:(bb+subsequenceLength-1)]
+      
+      AV[bb] = sqrt(sum(diff(subsequence)^2))
+      
+    }
+    
+    AV <- AV-min(AV)
+    AV <- AV/max(AV) 
+    
+    return(AV)
+    
+  }
+>>>>>>> Stashed changes
   
   return( list(AV = AV,
                stdVector = stdVector,
@@ -21,6 +64,7 @@ make_AV <- function(data, subsequenceLength){
   )
 }
 
+<<<<<<< Updated upstream
 # debug(make_AV)
 # make_AV(a,w)
 # 
@@ -36,4 +80,14 @@ make_AV <- function(data, subsequenceLength){
 # av <- make_AV(data,w)
 # 
 # plot(av$AV, type = "l")
+=======
+debug(make_AV)
+make_AV(a,w,'complexity')
+
+
+
+
+
+
+>>>>>>> Stashed changes
 
