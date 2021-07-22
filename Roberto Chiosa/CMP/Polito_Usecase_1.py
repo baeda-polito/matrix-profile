f96799472e610d11e434099f6453a28f47bc9d70
RobertoChiosa <roberto.chiosa@gmail.com>
2021-07-22 16:49:44 +0200

    added generalization to code

--- Roberto Chiosa/CMP/Polito_Usecase_1.py
+++ Roberto Chiosa/CMP/Polito_Usecase_1.py
@@ -11,10 +11,66 @@ from distancematrix.generator import ZNormEuclidean, Euclidean
 from distancematrix.consumer import MatrixProfileLR, ContextualMatrixProfile
 from distancematrix.consumer.contextmanager import GeneralStaticManager
 
+from matplotlib import rc  # font plot
+from kneed import KneeLocator  # find knee of curve
+
 path_to_data = 'Polito_Usecase/data/'
 path_to_figures = 'Polito_Usecase/figures1/'
 color_palette = 'viridis'
 dpi_resolution = 300
+fontsize = 10
+# plt.style.use("seaborn-paper")
+rc('font', **{'family': 'serif', 'serif': ['Georgia']})
+plt.rcParams.update({'font.size': fontsize})
+rc('text', usetex=True)
+
+
+def CMP_plot(cmp, palette="viridis", title=None, xlabel=None, legendlabel=None, extent=None):
+    """
+    utils function used to plot the contextual matrix profile
+
+    :param cmp:
+    :param extent:
+    :param palette:
+    :param title:
+    :param xlabel:
+    :param legendlabel:
+    :return:
+    """
+
+    if extent is not None:
+        # no extent dates given
+        plt.imshow(cmp,
+                   cmap=palette,
+                   origin="lower",
+                   extent=extent
+                   )
+        # Label layout
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        plt.gca().yaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+        plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(14))
+        plt.gca().yaxis.set_major_locator(mticker.MultipleLocator(14))
+        plt.gcf().autofmt_xdate()
+
+    else:
+        # index as
+        plt.imshow(cmp,
+                   cmap=palette,
+                   origin="lower",
+                   vmin=np.min(cmp),
+                   vmax=np.max(cmp)
+                   )
+        plt.xlabel(xlabel)
+        ticks = list(range(0, len(cmp), int(len(cmp) / 5)))
+        plt.xticks(ticks)
+        plt.yticks(ticks)
+
+    cbar = plt.colorbar()
+
+    plt.title(title)
+    cbar.set_label(legendlabel)
+
+
 ########################################################################################
 # load dataset
 data = pd.read_csv(path_to_data + "polito.csv", index_col='timestamp', parse_dates=True)
@@ -28,8 +84,8 @@ print(' POLITO CASE STUDY\n',
       '- From\t', data.index[0], '\n',
       '- To\t', data.index[len(data) - 1], '\n',
       '-', len(data), 'observations every 15 min\n',
-      '-',obs_per_day,'\t observations per day\n',
-      '-',obs_per_hour,'\t observations per hour\n'
+      '-', obs_per_day, '\t observations per day\n',
+      '-', obs_per_hour, '\t observations per hour\n'
       )
 
 # Visualise the data
@@ -76,11 +132,12 @@ plt.savefig(path_to_figures + "polito.png", dpi=dpi_resolution, bbox_inches='tig
 # context time window length (hours)
 m_context = 2
 
-# time window length
-m = obs_per_day - obs_per_hour * m_context  # 22 hours
+# time window length 96 observations - 4 observation/hour * 2 hours = 88 observations = 22 hours
+m = obs_per_day - obs_per_hour * m_context
 
-# Each context starts between 0 and 2 AM, and lasts 22 hours
-contexts = GeneralStaticManager([range(x * obs_per_day, (x * obs_per_day) + obs_per_hour * m_context) for x in range(len(data) // obs_per_day)])
+# Each context starts between 0 and 2 AM (m_context = 2 hours), and lasts 22 hours (m = 88 observations)
+contexts = GeneralStaticManager(
+    [range(x * obs_per_day, (x * obs_per_day) + obs_per_hour * m_context) for x in range(len(data) // obs_per_day)])
 
 calc = AnytimeCalculator(m, data.values.T)
 
@@ -88,7 +145,6 @@ calc = AnytimeCalculator(m, data.values.T)
 # - OPTION 1
 # distance_string = 'Znormalized Euclidean Distance'
 # calc.add_generator(0, ZNormEuclidean())
-
 # - OPTION 2
 distance_string = 'Not Normalized Euclidean Distance'
 calc.add_generator(0, Euclidean())
@@ -106,34 +162,37 @@ calc.calculate_columns()
 # Visualise the whole CMP
 
 # calculate the date labels to define the extent of figure
-date_labels = mdates.date2num(data.index[::48 * 2].values)
+date_labels = mdates.date2num(data.index[::m].values)
 
 # plot CMP as matrix
 plt.figure(figsize=(10, 10))
-extents = [date_labels[0], date_labels[-1], date_labels[0], date_labels[-1]]
-plt.imshow(cmp.distance_matrix, extent=extents, cmap=color_palette, origin="lower")
-cbar = plt.colorbar()
-plt.title('Contextual Matrix Profile\n'+distance_string+'\n')
 
-# Label layout
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.gca().yaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(14))
-plt.gca().yaxis.set_major_locator(mticker.MultipleLocator(14))
-plt.gcf().autofmt_xdate()
-cbar.set_label(distance_string)
+extents = [date_labels[0], date_labels[-1], date_labels[0], date_labels[-1]]
+CMP_plot(cmp=cmp.distance_matrix,
+         palette=color_palette,
+         title='Contextual Matrix Profile\n' + distance_string + '\n',
+         legendlabel=distance_string,
+         extent=extents,
+         )
+
 
 plt.savefig(path_to_figures + "polito_cmp1.png", dpi=dpi_resolution, bbox_inches='tight')
 
 ########################################################################################
 # Create boolean arrays to indicate whether each day is a weekday/weekend/saturday/sunday
+# - holiday (calendar holidays and academic calendar closures)
+# - saturdays (not holidays not working days)
+# - workingdays (not holiday and not saturdays)
 
+# load data
 annotation_df = pd.read_csv(path_to_data + "polito_holiday.csv", index_col='timestamp', parse_dates=True)
 
+# define arrays
 holiday = np.array(annotation_df.T)[0]
 saturdays = np.array(annotation_df.T)[1]
 workingdays = np.array(annotation_df.T)[2]
 
+# set labels
 day_labels = data.index[::obs_per_day]
 
 # Create weekday/weekend only CMP
@@ -169,72 +228,49 @@ cmp_ad_score[workingdays] = cmp_workingdays_score
 # Ordering of all days, from most to least anomalous
 ad_order = np.argsort(cmp_ad_score)[::-1]
 
-num_anomalies_to_show = 15
+# set number of aomalies to show as the elbow of the curve
+x_ad = np.array(range(0, len(cmp_ad_score)))
+y_ad = cmp_ad_score[ad_order]
+kn = KneeLocator(x_ad, y_ad, curve='convex', direction='decreasing')
+num_anomalies_to_show = kn.knee
 
 # Plot the anomaly scores and our considered threshold
-plt.figure(figsize=(15, 3))
+plt.figure(figsize=(10, 5))
 plt.title("Sorted Anomaly Scores")
 plt.plot(cmp_ad_score[ad_order])
 plt.ylabel("Anomaly Score")
-
 plt.axvline(num_anomalies_to_show, ls=":", c="gray")
-plt.xticks([0, 10, 20, 50, 100, 150])
-plt.show()
+anomaly_ticks = list(range(0, len(ad_order), int(len(ad_order) / 5)))
+anomaly_ticks.append(num_anomalies_to_show)
+plt.xticks(anomaly_ticks)
 
-# Plot the above figures together
-plt.figure(figsize=(17, 2.5))
-plt.subplot(1, 4, 1)
-date_labels = mdates.date2num(holiday_dates)
-extents = [date_labels[0], date_labels[-1], date_labels[0], date_labels[-1]]
-plt.imshow(holiday_cmp,
-           cmap=color_palette,
-           origin="lower",
-           vmin=np.min(holiday_cmp),
-           vmax=np.max(holiday_cmp),
-           )
-cbar = plt.colorbar()
-plt.xlabel("Holiday Index")
-# plt.yticks([0, 50, 100, 150])
-plt.title("Power CMP (holiday only)")
-cbar.set_label("Distance")
+plt.savefig(path_to_figures + "polito_aggregated_as.png", dpi=dpi_resolution, bbox_inches='tight')
 
-plt.subplot(1, 4, 2)
-date_labels = mdates.date2num(saturday_dates)
-extents = [date_labels[0], date_labels[-1], date_labels[0], date_labels[-1]]
-plt.imshow(saturday_cmp,
-           cmap=color_palette,
-           origin="lower",
-           vmin=np.min(saturday_cmp),
-           vmax=np.max(saturday_cmp),
-           )
-cbar = plt.colorbar()
-plt.xlabel("Saturday Index")
-# plt.yticks([0, 10, 20, 30])
-plt.title("Power CMP (Saturday only)")
-cbar.set_label("Distance")
+# Plot the CMP together
+plt.figure(figsize=(10, 4))
 
-plt.subplot(1, 4, 3)
-date_labels = mdates.date2num(workingdays_dates)
-extents = [date_labels[0], date_labels[-1], date_labels[0], date_labels[-1]]
-plt.imshow(workingdays_cmp,
-           cmap=color_palette,
-           origin="lower",
-           vmin=np.min(workingdays_cmp),
-           vmax=np.max(workingdays_cmp),
-           )
-cbar = plt.colorbar()
-plt.xlabel("Working days Index")
-# plt.yticks([0, 10, 20])
-plt.title("Power CMP (Working days only)")
-cbar.set_label("Distance")
+plt.subplot(1, 3, 1)
+CMP_plot(cmp=holiday_cmp,
+         palette=color_palette,
+         title="Power CMP (holiday only)",
+         xlabel="Holiday Index",
+         legendlabel="Distance")
 
-plt.subplot(1, 4, 4)
-plt.title("Sorted Anomaly Scores")
-plt.plot(cmp_ad_score[ad_order])
-plt.ylabel("Anomaly Score")
+plt.subplot(1, 3, 2)
+CMP_plot(cmp=saturday_cmp,
+         palette=color_palette,
+         title="Power CMP (Saturday only)",
+         xlabel="Saturday Index",
+         legendlabel="Distance")
 
-plt.axvline(10, ls=":", c="gray")
-# plt.xticks([0, 18, 50, 100, 150, 200])
+plt.subplot(1, 3, 3)
+CMP_plot(cmp=workingdays_cmp,
+         palette=color_palette,
+         title="Power CMP (Working days  only)",
+         xlabel="Working days  Index",
+         legendlabel="Distance")
+
+plt.tight_layout()
 
 plt.savefig(path_to_figures + "polito_cmp_detail1.png", dpi=dpi_resolution, bbox_inches='tight')
 
@@ -276,21 +312,28 @@ for i in range(num_anomalies_to_show):
 
     ax[i, 0].plot(data.values.reshape((-1, obs_per_day)).T, c="gray", alpha=0.07)
     ax[i, 0].plot(data.values[anomaly_range], c="red")
+    ax[i, 0].set_ylim([0, 850])
+    ax[i, 0].set_yticks([0, 200, 400, 600, 800])
 
     ax[i, 1].plot(data.values.reshape((-1, obs_per_day))[holiday].T, c="gray", alpha=0.07)
     ax[i, 1].plot(data.values[anomaly_range], c="red", linestyle=ls1)
+    ax[i, 1].set_ylim([0, 850])
+    ax[i, 1].set_yticks([0, 200, 400, 600, 800])
 
     ax[i, 2].plot(data.values.reshape((-1, obs_per_day))[saturdays].T, c="gray", alpha=0.07)
     ax[i, 2].plot(data.values[anomaly_range], c="red", linestyle=ls2)
+    ax[i, 2].set_ylim([0, 850])
 
     ax[i, 3].plot(data.values.reshape((-1, obs_per_day))[workingdays].T, c="gray", alpha=0.07)
     ax[i, 3].plot(data.values[anomaly_range], c="red", linestyle=ls3)
+    ax[i, 3].set_ylim([0, 850])
+    ax[i, 3].set_yticks([0, 200, 400, 600, 800])
 
-    ax[i, 0].text(0, 650, "CMP-Anomaly " + str(i + 1))
-    ax[i, date_col].text(0, 650, date.day_name() + " " + str(date)[:10])
+    ax[i, 0].text(0, position_y, "CMP-Anomaly " + str(i + 1))
+    ax[i, date_col].text(0, position_y, date.day_name() + " " + str(date)[:10])
 
 ax[0, 0].set_xticks(range(0, 97, 24))
-ticklabels = ["{hour}:00".format(hour=(x // 4)) for x in range(0, 97, 24)]
+ticklabels = ["{hour}:00".format(hour=(x // obs_per_hour)) for x in range(0, 97, 24)]
 ticklabels[-1] = ""
 ax[0, 0].set_xticklabels(ticklabels)
 
--- Roberto Chiosa/R _project/00-dataset-preprocessing.R
+++ Roberto Chiosa/R _project/00-dataset-preprocessing.R
@@ -3,32 +3,57 @@ cat("\014")                 # clears the console
 rm(list = ls())             # remove all variables of the workspace
 source(file = "00-setup.R") # load user functions
 
-df <- read.csv('./data/df_cabinaC_power_meteo_2015_2019.csv', sep = ',') %>%
+df_power <- read.csv('/Users/robi/Dropbox (Politecnico Di Torino Studenti)/aSAX/df_power.csv', sep = ',') %>%
   mutate(
     Date_Time = as.POSIXct(Date_Time , format = "%Y-%m-%d %H:%M:%S" , tz = "GMT"),
     Date = as.Date(Date_Time),
     Time = as.factor(Time),
     ToU = as.factor(FasciaAEEG),
-    Holiday = if_else(festivo == "S", TRUE, FALSE),
+    Holiday = if_else(festivo == "S", TRUE, FALSE)
+  ) %>%
+  dplyr::arrange(Date_Time) %>%
+  dplyr::select(-c("festivo", "FasciaAEEG"))
+
+df_meteo <- read.csv('/Users/robi/Dropbox (Politecnico Di Torino Studenti)/aSAX/df_clima.csv', sep = ',') %>%
+  mutate(
+    Date_Time = as.POSIXct(Date_Time , format = "%Y-%m-%d %H:%M:%S" , tz = "GMT"),
     T_air = TempARIA,
     H_global = RadGLOBale
   ) %>%
-  dplyr::select(-c("TempARIA", "RadGLOBale", "festivo", "FasciaAEEG"))
+  dplyr::arrange(Date_Time) %>%
+  dplyr::select("Date_Time", "T_air", "H_global")
 
 # extract colnames in order of magnitude to sort df
-tmp <- df %>%
+tmp <- df_power %>%
   select(is.numeric) %>% # select only numeric
-  select(-H_global, -T_air, -min_dec, -Day_Type) %>%
+  select( -min_dec, -Day_Type) %>%
   summarise(across(everything(), list(mean = mean))) %>% # summarise mean
   t() %>%
   as.data.frame() %>%
   arrange(desc(V1))
 
 
+# create a fill dataframe
+start <- df_power$Date_Time[1]
+interval <- 15
 
-df <- df %>%
-  select(colnames(df)[1:5], "T_air", "H_global", gsub("_mean", "", rownames(tmp)))
+end <- start + as.difftime(length(unique(df_power$Date)), units="days")
+
+ttt <- data.frame(Date_Time = seq(from=start, by=interval*60, to=end))
+
+
+df_py <- merge.data.frame(ttt, df_power, all.x =  T)
+
+df_py <- merge.data.frame(df_py, df_meteo, all.x =  T)
+
+
+summary(df_py)
+
+
+df_py <- df_py %>%
+  select(colnames(df_py)[1:5], "T_air", "H_global", gsub("_mean", "", rownames(tmp)))
   
-write.csv(df, "./data/df_cabinaC_full.csv")
+
+write.csv(df_py, "./data/df_cabinaC_full.csv")
 
             
