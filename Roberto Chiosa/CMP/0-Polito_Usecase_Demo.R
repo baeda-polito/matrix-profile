638053f9b2d7ec74a76c941c09c31342b27464a3
Roberto Chiosa <roberto.chiosa@gmail.com>
2021-09-17 18:54:11 +0200

    draft updated

--- Roberto Chiosa/CMP/0-Polito_Usecase_Demo.R
+++ Roberto Chiosa/CMP/0-Polito_Usecase_Demo.R
@@ -13,6 +13,7 @@ library(tidyverse)
 library(ggpubr)         # arrange plots
 library(ggtext)         # annotate text
 
+figma_palette <- c("#5EE393", "#0074FF", "#FF0005")
 
 df <- read.csv(file.path(dirname(dirname(getwd())), 
                          "Roberto Chiosa", 
@@ -26,41 +27,57 @@ df <- read.csv(file.path(dirname(dirname(getwd())),
                 value = as.numeric(Power_total)
   ) %>%
   dplyr::filter(timestamp >"2015-05-01" & timestamp <"2015-06-10") %>%
-  dplyr::select(timestamp, value)
+  dplyr::select(timestamp, value) %>%
+  # add subsequences
+  mutate(tag = ifelse(
+    timestamp >= "2015-05-03 00:00:00" & timestamp <= "2015-05-04 00:00:00", 
+    "Subsequence 1",
+    ifelse(
+      timestamp >= "2015-05-17 00:00:00" & timestamp <= "2015-05-18 00:00:00",
+      "Subsequence 2",
+      ifelse(
+        timestamp >= "2015-06-04 15:00:00" & timestamp <= "2015-06-05 15:00:00",
+        "Subsequence 3", NA
+      )
+    )
+  ),
+  tag = as.factor(tag)) 
+  
 
 
 fontsize <- 10
 linesize <- 0.7
 
-# # identification of subsequences
-# ggplot(df) +
-#   geom_line(
-#     aes(x = timestamp, y = value),
-#     color = "blue",
-#     size = linesize
-#   ) + 
-#   geom_line(
-#     data = df %>% filter(timestamp >= "2015-05-03 00:00:00",
-#                          timestamp <= "2015-05-04 00:00:00") ,
-#     aes(x = timestamp, y = value),
-#     color = "red",
-#     size = linesize
-#   )+
-#   geom_line(
-#     data = df %>% filter(timestamp >= "2015-05-17 00:00:00",
-#                          timestamp <= "2015-05-18 00:00:00") ,
-#     aes(x = timestamp, y = value),
-#     color = "red",
-#     size = linesize
-#   )+
-#   geom_line(
-#     data = df %>% filter(timestamp >= "2015-06-04 15:00:00",
-#                          timestamp <= "2015-06-05 15:00:00") ,
-#     aes(x = timestamp, y = value),
-#     color = "red",
-#     size = linesize
-#   )
+# identification of subsequences
 
+fig_all <- ggplot(df) +
+  geom_line(
+    aes(x = timestamp, y = value),
+    color = "gray",
+    size = linesize,
+    show.legend = F
+  ) +
+  geom_line(
+    data = df %>% filter(!is.na(tag)),
+    aes(x = timestamp, y = value, color = tag),
+    size = linesize
+  ) +
+  ylab("Not Normalized Power [kW]")+
+  scale_color_manual(values = figma_palette) +
+  theme_classic() +
+  theme(
+    text = element_text(family = font_family),
+    axis.title.x = element_blank(),
+    #axis.title.y = element_text(size =fontsize_smaller),
+    axis.title.y = element_blank(),
+    panel.background = element_rect(fill =  background_fill),
+    plot.background = element_rect(fill = background_fill, color = NA), # bg of the plot
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.title = element_blank(),
+    legend.background = element_rect(fill = background_fill, color = NA), # bg of the plot
+  ) +
+  scale_x_datetime(expand = c(0, 0), date_breaks = "1 week", date_labels = "%b-%d")
 
 
 seq1 = df %>% filter(timestamp >= "2015-05-03 01:00:00",
@@ -86,18 +103,20 @@ fig_pure <- ggplot(df_seq) +
     size = linesize
     
   ) + 
-  scale_color_brewer(palette = "Set1") +
+  ylab("Not Normalized Power [kW]")+
+  scale_color_manual(values = figma_palette) +
   theme_classic() +
   theme(
     text = element_text(family = font_family),
     axis.title.x = element_blank(),
+    #axis.title.y = element_text(size =fontsize_smaller),
     axis.title.y = element_blank(),
-    panel.background = element_rect(fill =  "transparent"),
-    plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    panel.background = element_rect(fill =  background_fill),
+    plot.background = element_rect(fill = background_fill, color = NA), # bg of the plot
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     legend.title = element_blank(),
-    legend.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    legend.background = element_blank()
   ) +
   scale_x_continuous(limits = c(1, L), expand = c(0, 0))
 
@@ -110,53 +129,67 @@ fig_z <- ggplot(df_seq) +
     size = linesize
     
   ) + 
+  ylab("Z-Normalized Power [-]")+
   theme_classic() +
-  scale_color_brewer(palette = "Set1") +
+  scale_color_manual(values = figma_palette) +
   theme(
     text = element_text(family = font_family),
     axis.title.x = element_blank(),
+    #axis.title.y = element_text(size =fontsize_smaller),
     axis.title.y = element_blank(),
-    panel.background = element_rect(fill =  "transparent"),
-    plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    panel.background = element_rect(fill =  background_fill),
+    plot.background = element_rect(fill = background_fill, color = NA), # bg of the plot
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     legend.title = element_blank(),
-    legend.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    legend.background = element_blank()
   ) +
   scale_x_continuous(limits = c(1, L), expand = c(0, 0))
 
 
-fig <-  ggarrange(
-  fig_pure, fig_z,
-  ncol = 2,
-  nrow = 1,
-  widths = c(3,3),
-  align = "v",
+fig <- ggarrange(
+  fig_all,
+  ggarrange(
+    fig_z,fig_pure,
+    ncol = 2,
+    widths = c(3,3),
+    align = "v",
+    legend="none"
+  ),
+  ncol = 1,
+  nrow = 2,
+  widths = c(1),
+  align = "h",
   common.legend = TRUE,
   legend="top"
 )
 
 
+
+
 dev.new()
-annotate_figure(fig,
+#annotate_figure(fig
                 # top = text_grob(paste("A", "discovery window profile"), color = "black", face = "bold", size = 13,  family = font_family),
-                left = text_grob("Not Normalized Power [kW]", color = "black", size = 11, rot = 90,  family = font_family),
-                right = text_grob("Z-Normalized Power [-]", color = "black",  size = 11, rot = 90,  family = font_family),
-                bottom = text_grob("Obs. Index", color = "black",  size = 11,  family = font_family)
+                #right = text_grob("Not Normalized Power [kW]", color = "black", size = 11, rot = 90,  family = font_family),
+                #left = text_grob("Z-Normalized Power [-]", color = "black",  size = 11, rot = 90,  family = font_family),
+                #bottom = text_grob("Obs. Index", color = "black",  size = 11,  family = font_family)
                 
-)
+#)
 
+fig
 
-ggsave(filename = file.path("Polito_Usecase", "figures", "demo", "znorm.png"), width = 7, height = 3.5, dpi = dpi )
+ggsave(filename = file.path("Polito_Usecase", "figures", "demo", "znorm.jpg"), 
+       width = 7, height = 4, dpi = dpi, bg = background_fill)
 
 dev.off()
 
 ############  D. De Paepe et al. FIG 1
 
+set.seed(123)
 L = 50
 seq1 = 1 + runif(L)
 seq2 = 6 + runif(L)
-distance1 <- dist(rbind(seq1,seq2) , method = "euclidean")
+distance1 <- dist(rbind(znorm(seq1),znorm(seq2)) , method = "euclidean")
 
 df_seq <- data.frame(
   value = c(seq1, seq2),
@@ -172,18 +205,18 @@ fig_pure <- ggplot(df_seq) +
     size = linesize
     
   ) + 
-  scale_color_brewer(palette = "Set1") +
+  scale_color_manual(values = figma_palette) +
   theme_classic() +
   theme(
     text = element_text(family = font_family),
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
-    panel.background = element_rect(fill =  "transparent"),
-    plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    panel.background = element_rect(fill =  background_fill),
+    plot.background = element_rect(fill = background_fill, color = NA), # bg of the plot
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     legend.position = "none",
-    legend.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    legend.background = element_rect(fill = background_fill, color = NA), # bg of the plot
   ) +
   scale_x_continuous(limits = c(0, L), expand = c(0, 0)) + 
   scale_y_continuous(limits = c(0, 14), expand = c(0, 0))
@@ -198,24 +231,24 @@ fig_z <- ggplot(df_seq) +
     
   ) + 
   theme_classic() +
-  scale_color_brewer(palette = "Set1") +
+  scale_color_manual(values = figma_palette) +
   theme(
     text = element_text(family = font_family),
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
-    panel.background = element_rect(fill =  "transparent"),
-    plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    panel.background = element_rect(fill =  background_fill),
+    plot.background = element_rect(fill = background_fill, color = NA), # bg of the plot
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     legend.position = "none",
-    legend.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    legend.background = element_rect(fill = background_fill, color = NA), # bg of the plot
   ) +
   scale_x_continuous(limits = c(0, L), expand = c(0, 0))+ 
   scale_y_continuous(limits = c(-2.5, 2.5), expand = c(0, 0))
 
 seq1 = seq1 + seq(L)/7 
 seq2 = seq2 + seq(L)/7 
-distance2 <- dist(rbind(seq1,seq2) , method = "euclidean")
+distance2 <- dist(rbind(znorm(seq1),znorm(seq2)) , method = "euclidean")
 
 df_seq <- data.frame(
   value = c(seq1, seq2),
@@ -231,18 +264,18 @@ fig_pure1 <- ggplot(df_seq) +
     size = linesize
     
   ) + 
-  scale_color_brewer(palette = "Set1") +
+  scale_color_manual(values = figma_palette) +
   theme_classic() +
   theme(
     text = element_text(family = font_family),
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
-    panel.background = element_rect(fill =  "transparent"),
-    plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    panel.background = element_rect(fill =  background_fill),
+    plot.background = element_rect(fill = background_fill, color = NA), # bg of the plot
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     legend.position = "none",
-    legend.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    legend.background = element_rect(fill = background_fill, color = NA), # bg of the plot
   ) +
   scale_x_continuous(limits = c(0, L), expand = c(0, 0))+ 
   scale_y_continuous(limits = c(0, 14), expand = c(0, 0))
@@ -257,17 +290,17 @@ fig_z1 <- ggplot(df_seq) +
     
   ) + 
   theme_classic() +
-  scale_color_brewer(palette = "Set1") +
+  scale_color_manual(values = figma_palette) +
   theme(
     text = element_text(family = font_family),
     axis.title.x = element_blank(),
     axis.title.y = element_blank(),
-    panel.background = element_rect(fill =  "transparent"),
-    plot.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    panel.background = element_rect(fill =  background_fill),
+    plot.background = element_rect(fill = background_fill, color = NA), # bg of the plot
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
     legend.position = "none",
-    legend.background = element_rect(fill = "transparent", color = NA), # bg of the plot
+    legend.background = element_rect(fill = background_fill, color = NA), # bg of the plot
   ) +
   scale_x_continuous(limits = c(0, L), expand = c(0, 0))+ 
   scale_y_continuous(limits = c(-2.5, 2.5), expand = c(0, 0))
@@ -294,6 +327,7 @@ annotate_figure(fig,
 )
 
 
-ggsave(filename = file.path("Polito_Usecase", "figures", "demo", "znorm_effects.png"), width = 7, height = 3.5, dpi = dpi )
+ggsave(filename = file.path("Polito_Usecase", "figures", "demo", "znorm_effects.jpg"), 
+       width = 7, height = 4, dpi = dpi, bg = background_fill)
 
 dev.off()
--- Roberto Chiosa/CMP/global_vars.R
+++ Roberto Chiosa/CMP/global_vars.R
@@ -2,10 +2,13 @@
 # 
 
 # windowsFonts("Georgia" = windowsFont("Georgia"))
-font_family <-  "Georgia"
+font_family <-  "Poppins"
 fontsize_large <- 12
 fontsize_medium <- 11
 fontsize_small <- 10
+fontsize_smaller <- 5
+background_fill <- "white"
+
 dpi <- 300
 palette <- rev(c('#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd'))
 #palette <- rev(RColorBrewer::brewer.pal(11,"RdYlBu"))
