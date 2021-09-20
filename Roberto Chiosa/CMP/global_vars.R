d033657a9fcf36703d5170e2224cae4320e6e315
Roberto Chiosa <roberto.chiosa@gmail.com>
2021-09-20 18:57:09 +0200

    updated figures and paper

--- Roberto Chiosa/CMP/global_vars.R
+++ Roberto Chiosa/CMP/global_vars.R
@@ -8,6 +8,7 @@ fontsize_medium <- 11
 fontsize_small <- 10
 fontsize_smaller <- 5
 background_fill <- "white"
+linesize <- 0.7
 
 dpi <- 300
 palette <- rev(c('#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd'))
